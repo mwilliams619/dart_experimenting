06452e28501b0083e292597aff1afdcac7030060
mwilliams619 <68926889+mwilliams619@users.noreply.github.com>
2022-01-03 12:09:41 -0500

    Delete creative_view directory

--- creative_view/lib/constants.dart
+++
@@ -1,7 +0,0 @@
-class Constants {
-  static const String Search = 'Search';
-  static const String Settings = 'Settings';
-  static const String Theme = 'Theme';
-
-  static const List<String> choices = <String>[Search, Settings, Theme];
-}
